d41cbd6fafca672a91743ef220b846b24c6c8a23
wzheng <hb_wangzheng@163.com>
2018-04-28 18:37:31 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App'
-
+import MandMobile from 'mand-mobile'
 Vue.config.productionTip = false
-App.mpType = 'app'
-
+App.mpType = 'app';
+// 全量引入
+Vue.use(MandMobile)
 const app = new Vue(App)
 app.$mount()
 
